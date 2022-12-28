d8f751204480a967fdfc9734178fdffe630b2b0e
Piyush Kumar <pgh5566@gmail.com>
2022-12-28 19:27:28 +0530

    Adding changes upto video 11 - Add Bootstrap

---
+++ README.md
@@ -0,0 +1,8 @@
+# ZONA
+
+# LESSONS
+
+1. Introduction
+2. Install Tools
+3. Create React App
+4. Create Git Repository
---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import { Container, Navbar } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+// export default App;
